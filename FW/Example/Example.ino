391a54c43f4fb091d39a950e4875a40fddb54a23
zapzrat <zapzrat@gmail.com>
2020-12-07 14:20:52 +0700

    Add files via upload

---
+++ FW/Example/Example.ino
@@ -0,0 +1,26 @@
+const int waterLavel = 12;
+const int ledStatus =  13;
+
+int buttonState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Micro Project Thailand ~^.^~ ");
+
+  pinMode(ledStatus, OUTPUT);
+  pinMode(waterLavel, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(waterLavel);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledStatus, HIGH);
+    Serial.println("Status Water lavel : HIGH");
+  } else {
+    digitalWrite(ledStatus, LOW);
+    Serial.println("Status Water lavel : LOW");
+  }
+
+  delay(200);
+}
